da751038ac823d67c5646b4cc7071d8e82247a7d
Tomxuetao <15555030339@163.com>
2025-02-27 18:28:07 +0800

    fix: 🐛 fix bugs

--- packages/amap-utils/tsup.config.ts
+++ packages/amap-utils/tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup'
 export default defineConfig({
   dts: true,
   clean: true,
-  minify: false,
+  minify: true,
   splitting: false,
   format: ['esm', 'cjs'],
   entry: ['src/index.ts']
